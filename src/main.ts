1413ba6c9eeb47e42f64e75ec0710d240cb4b06f
zhujie <630796280@qq.com>
2020-04-10 18:53:49 +0800

    初始化

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+
+// 全局插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
