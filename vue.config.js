ee0d1772c6bb6496825348b1b743bc720797d8a6
zhujie <630796280@qq.com>
2020-04-19 20:48:30 +0800

    打包项目

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? './'
+        : '/',
     chainWebpack: (config) => {
         config.module
             .rule('scss')
