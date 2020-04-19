382079ffcd3d2cf90152e0f87b257afd5b0f4ab2
zhujie <630796280@qq.com>
2020-04-19 22:20:04 +0800

    修改路径

---
+++ dist/precache-manifest.57c890c76da570a21cb250a0b32fd64f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733c8555f06edbf9694c",
+    "url": "css/app.e8a50ef3.css"
+  },
+  {
+    "revision": "86f8a6cb5d22e9743082",
+    "url": "css/chunk-vendors.5dab7802.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d63421449ef9fa9548d164125e4739c4",
+    "url": "index.html"
+  },
+  {
+    "revision": "4231c935a713d3374fbc",
+    "url": "js/about.80a322ec.js"
+  },
+  {
+    "revision": "733c8555f06edbf9694c",
+    "url": "js/app.e70cf35e.js"
+  },
+  {
+    "revision": "86f8a6cb5d22e9743082",
+    "url": "js/chunk-vendors.d754da09.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
